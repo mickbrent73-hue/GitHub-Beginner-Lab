1404f570c2db62724014eed69327d7c0c9b8ebd3
mick-brent-baquillas <mickbrent73@gmail.com>
2026-09-13 21:25:05 +0800

    Update student profile output.

--- StudentProfile/Form1.Designer.cs
+++ StudentProfile/Form1.Designer.cs
@@ -37,9 +37,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(174, 178);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(378, 26);
+            this.label1.Size = new System.Drawing.Size(389, 26);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Student Profile — Mick Brent Baquillas";
+            this.label1.Text = "Student Profile — GitHub Beginner Lab.";
             // 
             // Form1
             // 
