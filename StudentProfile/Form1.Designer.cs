95b595c478eb93b1711b7408fc99933c5cc2fb57
mick-brent-baquillas <mickbrent73@gmail.com>
2026-09-14 20:26:18 +0800

    Fix student contact number formatting

--- StudentProfile/Form1.Designer.cs
+++ StudentProfile/Form1.Designer.cs
@@ -45,10 +45,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(169, 228);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(315, 26);
+            this.label2.Size = new System.Drawing.Size(369, 29);
             this.label2.TabIndex = 1;
             this.label2.Text = "Contact Number: 09109741719";
             // 
